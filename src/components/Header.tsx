a3f47da43be7c607f3a82d49523372947ef8051b
Zojo <hchoi2894@gmail.com>
2024-05-08 19:28:17 +0900

    add: header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <div>Welcome to Seoul!</div>
+      <div>Traveling Tip </div>
+      <div>Must Visit </div>
+      <div>Must Try </div>
+      <div>My Place </div>
+    </header>
+  );
+}
