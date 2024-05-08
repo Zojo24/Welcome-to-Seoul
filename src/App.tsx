ca4b924649b6f2b8c4e049486d770959d9165cd0
Zojo <hchoi2894@gmail.com>
2024-05-08 22:13:43 +0900

    add: footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
