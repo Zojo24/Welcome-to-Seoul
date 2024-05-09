78966b64097079cf3d57e0422d7afba5b2962f96
Zojo <hchoi2894@gmail.com>
2024-05-09 16:48:00 +0900

    add: main page css

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,24 +3,22 @@ import { Link } from "react-router-dom";
 export default function MainPage() {
   return (
     <div className="main-page">
-      <h1>
-        서울에 오신 것을 환영합니다! <br />
-        <br /> Welcome to Seoul!
-      </h1>
-      <div className="button">
-        <h3>
+      <h2>서울에 오신 것을 환영합니다!</h2>
+      <h1>Welcome to Seoul!</h1>
+      <div className="button1">
+        <div className="button__box--tips">
           <Link to="/traveling-tips">Traveling Tips</Link>
-        </h3>
+        </div>
       </div>
-      <div className="button">
-        <h3>
+      <div className="button2">
+        <div className="button__box">
           <Link to="/must-visit">Must Visit!</Link>
-        </h3>
+        </div>
       </div>
-      <div className="button">
-        <h3>
+      <div className="button3">
+        <div className="button__box">
           <Link to="/must-try">Must Try!</Link>
-        </h3>
+        </div>
       </div>
     </div>
   );
