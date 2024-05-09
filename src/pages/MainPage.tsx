876b6c6e642e927a60e1a800b4d63afcc36001e6
Zojo <hchoi2894@gmail.com>
2024-05-09 18:26:24 +0900

    add: main page animation

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,21 +3,27 @@ import { Link } from "react-router-dom";
 export default function MainPage() {
   return (
     <div className="main-page">
-      <h2>서울에 오신 것을 환영합니다!</h2>
-      <h1>Welcome to Seoul!</h1>
-      <div className="button1">
-        <div className="button__box--tips">
-          <Link to="/traveling-tips">Traveling Tips</Link>
-        </div>
+      <div className="title">
+        <h2>서울에 오신 것을 환영합니다!</h2>
+        <h1>Welcome to Seoul!</h1>
       </div>
-      <div className="button2">
-        <div className="button__box">
-          <Link to="/must-visit">Must Visit!</Link>
+      <div className="background1"></div>
+      <div className="background2"></div>
+      <div className="buttons">
+        <div className="button1">
+          <div className="button__box--tips">
+            <Link to="/traveling-tips">Traveling Tips</Link>
+          </div>
         </div>
-      </div>
-      <div className="button3">
-        <div className="button__box">
-          <Link to="/must-try">Must Try!</Link>
+        <div className="button2">
+          <div className="button__box">
+            <Link to="/must-visit">Must Visit!</Link>
+          </div>
+        </div>
+        <div className="button3">
+          <div className="button__box">
+            <Link to="/must-try">Must Try!</Link>
+          </div>
         </div>
       </div>
     </div>
