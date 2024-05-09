421e46ec20c284d2b5140eaab3b2f79d6c58175b
Zojo <hchoi2894@gmail.com>
2024-05-09 12:35:25 +0900

    feat: Main page 레이아웃 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import MainPage from "./components/MainPage";
 
 export default function App() {
   return (
     <>
       <Header />
+      <MainPage />
       <Footer />
     </>
   );
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -5,10 +5,22 @@ import TravelingTip from "./TravelingTip";
 export default function MainPage() {
   return (
     <div className="main-page">
-      <h1>Welcome to Seoul!</h1>
-      <TravelingTip />
-      <MustVisit />
-      <MustTry />
+      <h1>
+        서울에 오신 것을 환영합니다! <br />
+        <br /> Welcome to Seoul!
+      </h1>
+      <div className="button">
+        <h3>Traveling Tip</h3>
+        <TravelingTip />
+      </div>
+      <div className="button">
+        <h3>Must Visit!</h3>
+        <MustVisit />
+      </div>
+      <div className="button">
+        <h3>Must Try!</h3>
+        <MustTry />
+      </div>
     </div>
   );
 }
