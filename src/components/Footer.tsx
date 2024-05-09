d18152e58e39f4cb66eabb10b8396c7764e0ae3e
Zojo <hchoi2894@gmail.com>
2024-05-09 18:49:52 +0900

    modify: footer component 내용 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,18 @@
 export default function Footer() {
+  const thisYear = new Date().getFullYear();
   return (
     <footer>
-      <div>© 2024 Zojo</div>
+      <div>&copy; {thisYear} Welcome to Seoul! All Rights Reserved by Zojo</div>
+      <div>
+        Github:{" "}
+        <a
+          href="https://github.com/Zojo24"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit Zojo's Github
+        </a>
+      </div>
     </footer>
   );
 }
