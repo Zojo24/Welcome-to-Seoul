e07db10f25e2e8a576d8e0185bf4805dd769b740
Zojo <hchoi2894@gmail.com>
2024-05-10 16:24:10 +0900

    modify: 전체 레이아웃 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 export default function MainPage() {
   return (
     <div className="main-page">
-      <div className="title">
+      <div className="title-container">
         <h2>서울에 오신 것을 환영합니다!</h2>
         <h1>Welcome to Seoul!</h1>
+        <div className="background1"></div>
+        <div className="background2"></div>
       </div>
-      <div className="background1"></div>
-      <div className="background2"></div>
       <div className="buttons">
         <div className="button1">
           <div className="button__box--tips">
