c317e39c43325bd90678b00bdd4385278440ceb2
Zojo <hchoi2894@gmail.com>
2024-05-11 21:58:08 +0900

    add: must try scss

--- src/pages/MustTry.tsx
+++ src/pages/MustTry.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import Place from "../components/Place";
+import "./MustTry.scss";
 
 export default function MustTry() {
   const navigate = useNavigate();
@@ -10,7 +10,6 @@ export default function MustTry() {
     <div className="page_container">
       <h1>Must Try!</h1>
       <h2>When you are in Seoul, you must try...</h2>
-      <Place />
       <button className="blog-btn" onClick={onClick}>
         Write Blog
       </button>
