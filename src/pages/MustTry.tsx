8982302449f3182fe7c7fde271f023b142aa9f1a
Zojo <hchoi2894@gmail.com>
2024-05-11 21:52:04 +0900

    add: must try page 레이아웃

--- src/pages/MustTry.tsx
+++ src/pages/MustTry.tsx
@@ -1,3 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import Place from "../components/Place";
+
 export default function MustTry() {
-  return <></>;
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate("/blog-post");
+  };
+  return (
+    <div className="page_container">
+      <h1>Must Try!</h1>
+      <h2>When you are in Seoul, you must try...</h2>
+      <Place />
+      <button className="blog-btn" onClick={onClick}>
+        Write Blog
+      </button>
+    </div>
+  );
 }
