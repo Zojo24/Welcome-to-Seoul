d91b4050bcee7232d5026a978632ca3b1215a7da
Zojo <hchoi2894@gmail.com>
2024-05-12 22:47:26 +0900

    add: must visit page 레이아웃

--- src/pages/MustVisit.tsx
+++ src/pages/MustVisit.tsx
@@ -1,9 +1,17 @@
-import PostForm from "./PostForm";
+import { useNavigate } from "react-router-dom";
 
 export default function MustVisit() {
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate("/blog-post");
+  };
   return (
-    <>
-      <PostForm />
-    </>
+    <div className="page_container">
+      <h1>Must Visit!</h1>
+      <h2>When you are in Seoul, you must visit...</h2>
+      <button className="blog-btn" onClick={onClick}>
+        Write Blog
+      </button>
+    </div>
   );
 }
