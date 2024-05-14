4281864783dc7c619172874712d9fa98d4da6819
Zojo <hchoi2894@gmail.com>
2024-05-14 15:50:18 +0900

    modify: 폴더 구조 및 scss 파일 정리

--- firebaseApp.ts
+++ firebaseApp.ts
@@ -1,25 +1,21 @@
-import { initializeApp, getApp, FirebaseApp } from "firebase/app";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export let app: FirebaseApp;
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyCO7hmoy3_fnfZar7-cVOPZoLoeIo3v4fI",
+  authDomain: "welcome-to-seoul.firebaseapp.com",
+  projectId: "welcome-to-seoul",
+  storageBucket: "welcome-to-seoul.appspot.com",
+  messagingSenderId: "1096052679605",
+  appId: "1:1096052679605:web:d5a90b27b7728f7e67affc",
+  measurementId: "G-ZW6YYBTTGY",
 };
 
-try {
-  app = getApp("app");
-} catch (e) {
-  app = initializeApp(firebaseConfig, "app");
-}
-
-const firebase = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-export default firebase;
