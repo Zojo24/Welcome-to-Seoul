2a80fc750c694d965049464a0dccdaa74f886e65
Zojo <hchoi2894@gmail.com>
2024-05-15 15:23:54 +0900

    fix: Firebase 세팅

---
+++ firebaseApp.tsx
@@ -0,0 +1,28 @@
+import { FirebaseApp, getApp, initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+export let app: FirebaseApp;
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+try {
+  app = getApp("app");
+} catch (e) {
+  app = initializeApp(firebaseConfig, "app");
+}
+
+const firebase = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export default firebase;
+const analytics = getAnalytics(app);
