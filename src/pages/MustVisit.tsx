267083a376e7700f764abcb512b65e78b454dd14
Zojo <hchoi2894@gmail.com>
2024-05-16 11:55:02 +0900

    feat: 게시판 R 기능 (Must Visit page)

--- src/pages/MustVisit.tsx
+++ src/pages/MustVisit.tsx
@@ -1,10 +1,49 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../firebaseApp";
+import Place from "../components/Place";
+
+type PostProps = {
+  id: string;
+  placeEng: string;
+  placeKor?: string;
+  address: string;
+  comment: string;
+  rating: string;
+  category?: "Select!" | "Traveling Tips" | "Must Visit" | "Must Try";
+  recommendation?: string;
+};
 
 export default function MustVisit() {
+  const [posts, setPosts] = useState<PostProps[]>([]);
   const navigate = useNavigate();
   const onClick = () => {
     navigate("/blog-post");
   };
+
+  useEffect(() => {
+    const postsRef = collection(db, "posts");
+    const postsQuery = query(
+      postsRef,
+      where("category", "==", "Must Visit"),
+      orderBy("createdAt", "desc")
+    );
+    onSnapshot(postsQuery, (snapShot) => {
+      const dataObj = snapShot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })) as PostProps[];
+      setPosts(dataObj);
+    });
+  }, []);
+
   return (
     <div className="page_container">
       <h1>Must Visit!</h1>
@@ -12,6 +51,11 @@ export default function MustVisit() {
       <button className="blog-btn" onClick={onClick}>
         Write Blog
       </button>
+      <div className="places_list">
+        {posts.map((post) => (
+          <Place key={post.id} {...post} />
+        ))}
+      </div>
     </div>
   );
 }
