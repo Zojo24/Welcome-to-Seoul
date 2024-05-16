d2ae60a2b33cfc30a26254a825f3e1f6a1976902
Zojo <hchoi2894@gmail.com>
2024-05-16 19:00:40 +0900

    add: tip detail 페이지 css추가

--- src/pages/TipDetail.tsx
+++ src/pages/TipDetail.tsx
@@ -1,3 +1,46 @@
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { db } from "../firebaseApp";
+import "./TipDetail.scss";
+
+type TipProps = {
+  id: string;
+  title: string;
+  topic: string;
+  content?: string;
+};
+
 export default function TipDetail() {
-  return <></>;
+  const { id } = useParams<{ id: string }>();
+  const [post, setPost] = useState<TipProps | null>(null);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const docRef = doc(db, "Tips", id!);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        const data = docSnap.data();
+        setPost({
+          id: docSnap.id,
+          title: data.title,
+          topic: data.topic,
+          content: data.content,
+        } as TipProps);
+      }
+    };
+
+    fetchPost();
+  }, [id]);
+
+  if (!post) return <div>Loading...</div>;
+  return (
+    <div className="tip_detail">
+      <span className="tip_title">{post.title}</span>
+      <p className="tip_topic">Topic : {post.topic}</p>
+
+      <p className="tip_content">{post.content}</p>
+    </div>
+  );
 }
