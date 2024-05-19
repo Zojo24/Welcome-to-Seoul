f9c329ee9509c22c740afc88bcea6d2bdf23107f
Zojo <hchoi2894@gmail.com>
2024-05-19 19:22:17 +0900

    modify: Main page 버튼 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -10,21 +10,15 @@ export default function MainPage() {
         <div className="background2"></div>
       </div>
       <div className="buttons">
-        <div className="button1">
-          <div className="button__box--tips">
-            <Link to="/traveling-tips">Traveling Tips</Link>
-          </div>
-        </div>
-        <div className="button2">
-          <div className="button__box">
-            <Link to="/must-visit">Must Visit!</Link>
-          </div>
-        </div>
-        <div className="button3">
-          <div className="button__box">
-            <Link to="/must-try">Must Try!</Link>
-          </div>
-        </div>
+        <Link to="/must-visit" className="button1">
+          <div className="button__box">Must Visit!</div>
+        </Link>
+        <Link to="/must-try" className="button2">
+          <div className="button__box">Must Try!</div>
+        </Link>
+        <Link to="/traveling-tips" className="button3">
+          <div className="button__box--tips">Traveling Tips</div>
+        </Link>
       </div>
     </div>
   );
