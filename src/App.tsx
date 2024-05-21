e073b2cb68e9dcce02087141c30c8e1cd582b84c
Zojo <hchoi2894@gmail.com>
2024-05-21 10:23:50 +0900

    modify: app에 auth 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,35 @@ import Header from "./components/Header";
 import Router from "./router/Router";
 import ScrollToTop from "./components/ScrollToTop";
 import { ToastContainer } from "react-toastify";
-// import { getAuth } from "firebase/auth";
-// import { app } from "./firebaseApp";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "./firebaseApp";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  // const [authenticated, setAuthenticated] = useState<boolean>(false)
-  // const auth = getAuth(app);
+  const auth = getAuth(app);
+
+  const [init, setInit] = useState<boolean>(false);
+  const [authenticated, setAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthenticated(true);
+      } else {
+        setAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
 
   return (
     <BrowserRouter>
       <ScrollToTop />
       <ToastContainer />
       <Header />
-      <Router />
+      {init ? <Router authenticated={authenticated} /> : <>Loading...</>}
       <Footer />
     </BrowserRouter>
   );
